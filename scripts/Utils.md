73b73024d828efbe01d7bdc17cfe4dda7db8a190
mantheys <sergio.manthey@gmail.com>
2023-10-24 17:30:04 +0200

    UPDATE DOCS CONFIG

---
+++ scripts/Utils.md
@@ -0,0 +1,7 @@
+# Utils
+
+## Source code
+
+```bash
+/cvmfs/sft.cern.ch/lcg/releases/Python/3.7.3-f4f57/x86_64-centos7-gcc7-opt/bin/python3
+```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,9 @@
+# Important note: This script is meant to be run from the root of the repository!!!
+
 # Description: Setup the environment for the project
-source /cvmfs/sft.cern.ch/lcg/releases/Python/3.7.3-f4f57/x86_64-centos7-gcc7-opt/bin/python3
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
+# Mount the key directories (data, notebooks, sensitivity) to the right path in the repository
+ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/data data
+ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/notebooks notebooks 
+ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/sensitivity sensitivity
