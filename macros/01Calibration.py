e5a579b37babf96f22dbf691c1e8e111b86c60ac
mantheys <sergio.manthey@gmail.com>
2023-10-25 12:39:03 +0200

    UPDATE CALIBRATION FUNCTION TO PYROOT

--- macros/01Calibration.py
+++ macros/01Calibration.py
@@ -1,64 +1,67 @@
-# ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/data/ .
-
 # Load function libraries and set up the environment
 import sys; sys.path.insert(0, '../'); from lib.__init__ import *
 np.seterr(divide='ignore', invalid='ignore')
 plt.rcParams.update({'font.size': 15})
 
 # Load macro configuration
-default_dict = {}
-user_input = initialize_macro("00Process",["config_file","root_file","rewrite","debug"],default_dict=default_dict, debug=True)
+user_input = initialize_macro("01Calibration",["config_file","root_file","rewrite","debug"],default_dict={}, debug=True)
 user_input = check_macro_config(user_input,debug=user_input["debug"])
 
 # Format input file names and load analysis data
-config = user_input["config_file"].split("/")[-1].split("_config")[0]    
-config_files = {config:config+"_config"}
-names = {config:user_input["root_file"]}
+config = user_input["config_file"].split("/")[-1].split("_config")[0]
+info = read_input_file(user_input["config_file"],path="../config/",debug=user_input["debug"])
 
-truth_labels, reco_labels = get_workflow_branches(workflow="CALIBRATION",debug=False)
-run = load_multi(names,config_files,load_all=False,preset="",branches={"Truth":truth_labels,"Reco":reco_labels},debug=False)
+if user_input["debug"]: print_colored("\nLoading data...","DEBUG")
+input_file = TFile(info["PATH"][0]+info["NAME"][0]+user_input["root_file"][0]+".root")
+folder_name = input_file.GetListOfKeys()[0].GetName()
+tree = input_file.Get(folder_name + "/" + "SolarNuAnaTree")
+print_colored("-> Found tree: %s"%tree.GetName(), "SUCCESS")
 
-# Load analysis configuration
-analysis_info = read_input_file("analysis",INTEGERS=["RECO_ENERGY_RANGE","RECO_ENERGY_BINS","NADIR_RANGE","NADIR_BINS"],debug=False)
-energy_edges = np.linspace(analysis_info["RECO_ENERGY_RANGE"][0],analysis_info["RECO_ENERGY_RANGE"][1],analysis_info["RECO_ENERGY_BINS"][0]+1)
-energy_centers = (energy_edges[1:]+energy_edges[:-1])/2
-bin_width = energy_edges[1]-energy_edges[0]
-
-# Compute the calibration workflow
-run = compute_reco_workflow(run,config_files,workflow="CALIBRATION",debug=True)
-
-# Filter the data for calibration
 max_energy = 20; acc = 50
-info = read_input_file(config_files[config],path="../config/"+config+"/",debug=False)
-total_energy_filter = run["Reco"]["TNuE"] < max_energy*1e-3
-# electron_filter     = run["Reco"]["MarleyFrac"][:,0] > 0.9
-geo_filter          = np.asarray(run["Reco"]["Geometry"]) == info["GEOMETRY"][0]
-version_filter      = np.asarray(run["Reco"]["Version"]) == info["VERSION"][0]
-time_filter         = abs(run["Reco"]["Time"]) < info["EVENT_TICKS"][0]
-neutron_filter      = (run["Reco"]["TMarleyPDG"][:,:] != 2112).all(axis=1)
+calib_time = np.array([],dtype=float)
+calib_charge = np.array([],dtype=float)
+calib_energy = np.array([],dtype=float)
+calib_idx = np.array([],dtype=int)
+data = {"Primary":    np.zeros(tree.GetEntries(), dtype=bool),
+        "Correction": np.ones(tree.GetEntries(), dtype=float),
+        "Energy":     np.zeros(tree.GetEntries(), dtype=float),}
 
-filter1 = (total_energy_filter)*(geo_filter)*(version_filter)*(neutron_filter)*(time_filter)*(run["Reco"]["Primary"])
+for i in track(range(tree.GetEntries()),description="Filtering electron data..."):
+    tree.GetEntry(i)
+
+    # Primary computation
+    try: data["Primary"][i] = tree.Charge > max(tree.AdjClCharge)
+    except ValueError: data["Primary"][i] = False
+
+    if 1e3*tree.TNuE > max_energy: continue
+    if abs(tree.Time) > info["EVENT_TICKS"][0]: continue
+    if data["Primary"][i] == False: continue
+    if (np.asarray(tree.TMarleyPDG) == 2112).any(): continue # Very time consuming!
+
+    calib_time = np.append(calib_time,abs(tree.Time))
+    calib_charge = np.append(calib_charge,tree.Charge)
+    calib_energy = np.append(calib_energy,1e3*tree.TMarleyE[2])
+    calib_idx = np.append(calib_idx,i)
+print_colored("-> Found %i electron candidates out of %i events!"%(calib_idx.size,i),"SUCCESS")
 
 # Plot the calibration workflow
 fig = make_subplots(rows=2, cols=2,subplot_titles=("Time Profile","Correction","Charge Calibration"))
-if user_input["debug"]: print_colored("Filtering %d events"%np.sum(filter1),"SUCCESS")
-fig, corr_popt, perr = get_hist2d_fit(np.abs(run["Reco"]["Time"][filter1]),(run["Reco"]["Charge"]/(1e3*run["Reco"]["TMarleyE"][:,2]))[filter1],acc,fig,1,1,func_type="exponential",debug=user_input["debug"])
+fig, corr_popt, perr = get_hist2d_fit(calib_time,calib_charge/calib_energy,acc,fig,1,1,func_type="exponential",debug=user_input["debug"])
 
-# Save the true energy fit parameters to a txt file
-if not os.path.exists("../config/"+config+"/"+config+"_calib/"): os.makedirs("../config/"+config+"/"+config+"_calib/")
-with open("../config/"+config+"/"+config+"_calib/"+config+"_corr_config.txt",'w') as f:
-    f.write("CHARGE_AMP: %f\n"%corr_popt[0])
-    f.write("ELECTRON_TAU: %f\n"%corr_popt[1])
-plt.close()
+# Compute cluster energy
+for i in track(range(tree.GetEntries()),description="Computing cluster energy..."):
+    tree.GetEntry(i)
 
-run = compute_cluster_energy(run,config_files,params={"DEFAULT_ENERGY_TIME":"Time","DEFAULT_ADJCL_ENERGY_TIME":"AdjClTime"},debug=user_input["debug"])
+    # Energy computation
+    data["Correction"][i]  = np.exp(np.abs(tree.Time)/corr_popt[1])
+    data["Energy"][i] = tree.Charge*data["Correction"][i]/corr_popt[0]
 
-fig.add_trace(go.Histogram2d(x=run["Reco"]["Time"][filter1],
-    y=(run["Reco"]["Charge"]*run["Reco"]["Correction"]/(corr_popt[0]*1e3*run["Reco"]["TMarleyE"][:,2]))[filter1],
+fig.add_trace(go.Histogram2d(x=calib_time,
+    y=(calib_charge*(data["Correction"][calib_idx])/(corr_popt[0]*calib_energy)),
     coloraxis="coloraxis",nbinsx=acc,nbinsy=acc),row=1,col=2)
 
-fig, reco_popt, perr = get_hist2d_fit(1e3*run["Reco"]["TMarleyE"][:,2][filter1],run["Reco"]["Energy"][filter1],acc,fig,2,1,func_type="linear",debug=user_input["debug"])
-fig, res_popt, perr = get_hist1d_fit((run["Reco"]["Charge"]*run["Reco"]["Correction"]/(corr_popt[0]*1e3*run["Reco"]["TMarleyE"][:,2]))[filter1],2*acc,fig,2,2,func_type="gauss",debug=user_input["debug"])
+fig, reco_popt, perr = get_hist2d_fit(calib_energy,data["Energy"][calib_idx],acc,fig,2,1,func_type="linear",debug=user_input["debug"])
+fig, res_popt, perr = get_hist1d_fit(data["Energy"][calib_idx]/calib_energy,2*acc,fig,2,2,func_type="gauss",debug=user_input["debug"])
 
 fig.update_layout(coloraxis=dict(colorscale="Turbo",colorbar=dict(title="Counts")),showlegend=False,
     title="Calibration",
@@ -77,3 +80,10 @@ if not os.path.exists("../plots/calibration/%s_calibration/"%config): os.makedir
 fig.write_image("../plots/calibration/%s_calibration/%s_calibration.png"%(config,config), width=2400, height=1080)
 print_colored("-> Saved plots to ../plots/calibration/%s_calibration/%s_calibration.png"%(config,config),"SUCCESS")
 fig.show()
+
+# Save the true energy fit parameters to a txt file
+if not os.path.exists("../config/"+config+"/"+config+"_calib/"): os.makedirs("../config/"+config+"/"+config+"_calib/")
+with open("../config/"+config+"/"+config+"_calib/"+config+"_corr_config.txt",'w') as f:
+    f.write("CHARGE_AMP: %f\n"%corr_popt[0])
+    f.write("ELECTRON_TAU: %f\n"%corr_popt[1])
+print_colored("-> Saved calibration parameters to ../config/"+config+"/"+config+"_calib/"+config+"_corr_config.txt","SUCCESS")
