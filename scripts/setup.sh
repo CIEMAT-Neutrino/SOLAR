e6ca9ba16a825ec0e4053cd5e6030082821a0297
mantheys <sergio.manthey@gmail.com>
2023-11-03 22:48:13 +0100

    UPDATE SETUP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,29 @@
+#!/bin/bash
+
 # Important note: This script is meant to be run from the root of the repository!!!
 
 # Description: Setup the environment for the project
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+# source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+source /scr/neutrinos/rodrigoa/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
 # Mount the key directories (data, notebooks, sensitivity) to the right path in the repository
 if [ -L data ]; then
     echo "data symlink already exists"
 else
     echo "data symlink does not exist"
-    ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/data data
+    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/data data
 fi
 
 if [ -L notebooks ]; then
     echo "notebooks symlink already exists"
 else
     echo "notebooks symlink does not exist"
-    ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/notebooks notebooks 
+    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/notebooks notebooks
 fi
 
 if [ -L sensitivity ]; then
     echo "sensitivity symlink already exists"
 else
     echo "sensitivity symlink does not exist"
-    ln -s /pc/choozdsk01/palomare/DUNE/SOLAR/sensitivity sensitivity
+    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/sensitivity sensitivity
 fi
