9914d4b82c8b2861dc6af79437d4c93f8d6931bc
mantheys <sergio.manthey@gmail.com>
2023-11-04 10:48:34 +0100

    TEST

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # extensions = []
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'nbsphinx', 'sphinx_plotly_directive']
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx_plotly_directive']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
