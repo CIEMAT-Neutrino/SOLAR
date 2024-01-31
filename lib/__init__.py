42442ffdfaa4abf346f0b58581189066405058d9
mantheys <sergio.manthey@ciemat.es>
2024-01-31 23:36:28 +0100

    CLEANUP INIT

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,39 +1,13 @@
-import os, copy, uproot, math, random, gc, numba, ROOT
-import numpy                 as np
-import pandas                as pd
-import awkward               as ak
-import plotly.express        as px
-import plotly.offline        as pyoff
-import plotly.graph_objects  as go
-import matplotlib.cm         as cm
-import matplotlib.colors     as colors
-import matplotlib.pyplot     as plt
-import datashader            as ds
-
-from ROOT                    import TFile, RDataFrame
-from particle                import Particle
-from itertools               import product
-from scipy                   import interpolate
-from scipy                   import special as sp
-from scipy                   import constants as const
-from scipy.optimize          import curve_fit
-from plotly.tools            import FigureFactory
-from matplotlib.colors       import LogNorm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from plotly.subplots         import make_subplots
-from matplotlib.colors       import Normalize
-from rich.progress           import track
-from rich                    import print as rprint
-
-from .df_functions        import *
-from .fit_functions       import *
-from .geo_functions       import *
-from .head_functions      import *
-from .hit_functions       import *
-from .io_functions        import *
-from .osc_functions       import *
-from .plt_functions       import *
-from .reco_functions      import *
-from .root_functions      import *
-from .solar_functions     import *
-from .wkf_functions       import *
+from .df_functions import *
+from .fit_functions import *
+from .geo_functions import *
+from .head_functions import *
+from .hit_functions import *
+from .io_functions import *
+from .osc_functions import *
+from .plt_functions import *
+from .reco_functions import *
+from .root_functions import *
+from .solar_functions import *
+from .wkf_functions import *
+from .evt_functions import *
