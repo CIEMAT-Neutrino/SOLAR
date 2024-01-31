ac976f1882e2a613b7e8a615b2f5ec20b829e966
mantheys <sergio.manthey@ciemat.es>
2024-01-31 23:40:58 +0100

    CLEANUP SETUP SCRIPT

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,23 +7,13 @@
 # source /scr/neutrinos/rodrigoa/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
 # Mount the key directories (data, notebooks, sensitivity) to the right path in the repository
-if [ -L data ]; then
-    echo "data symlink already exists"
-else
-    echo "data symlink does not exist"
-    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/data data
-fi
+directories=("data" "notebooks" "sensitivity")
 
-if [ -L notebooks ]; then
-    echo "notebooks symlink already exists"
-else
-    echo "notebooks symlink does not exist"
-    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/notebooks notebooks
-fi
-
-if [ -L sensitivity ]; then
-    echo "sensitivity symlink already exists"
-else
-    echo "sensitivity symlink does not exist"
-    sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/sensitivity sensitivity
-fi
+for dir in "${directories[@]}"; do
+    if [ -L "$dir" ]; then
+        echo "$dir symlink already exists"
+    else
+        echo "generating symlink for $dir..."
+        sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/$dir "$dir"
+    fi
+done
