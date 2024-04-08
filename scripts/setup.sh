543954c9aa5cd599f9f25645c9981e7fa7ffbc91
mantheys <sergio.manthey@ciemat.es>
2024-04-08 13:17:28 +0200

    UPDATE SETUP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,14 +6,30 @@
 # source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 # source /scr/neutrinos/rodrigoa/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-# Mount the key directories (data, notebooks, sensitivity) to the right path in the repository
-directories=("data" "notebooks" "sensitivity")
+# Mount the key directories (notebooks, sensitivity) to the right path in the repository
+# directories=("notebooks")
+
+# for dir in "${directories[@]}"; do
+#     if [ -L "$dir" ]; then
+#         echo "$dir symlink already exists"
+#     else
+#         echo "unmounting $dir..."
+# 	    fusermount -uz "$dir"
+#         echo "generating symlink for $dir..."
+#         sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/users/manthey/SOLAR/$dir "$dir"
+#     fi
+# done
+
+# Mount the key directories (data) from pnfs to the right path in the repository
+directories=("data" "sensitivity")
 
 for dir in "${directories[@]}"; do
     if [ -L "$dir" ]; then
         echo "$dir symlink already exists"
     else
+        echo "unmounting $dir..."
+	    fusermount -uz "$dir"
         echo "generating symlink for $dir..."
-        sshfs manthey@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/DUNE/SOLAR/$dir "$dir"
+        sshfs manthey@gaeuidc1.ciemat.es:/pnfs/ciemat.es/data/neutrinos/DUNE/SOLAR/$dir "$dir"
     fi
 done
