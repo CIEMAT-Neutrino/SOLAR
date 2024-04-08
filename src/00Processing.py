30f2171b6f80fd0a57bebd6195b50557ccad9cc2
mantheys <sergio.manthey@ciemat.es>
2024-04-08 13:16:54 +0200

    MOVE FILES TO LIB

--- src/00Processing.py
+++ src/00Processing.py
@@ -7,9 +7,11 @@ from lib import (
     get_root_info,
     root2npy,
     remove_processed_branches,
+    get_project_root
 )
 
 default_dict = {}
+root = get_project_root()
 user_input = initialize_macro(
     "00Process",
     ["config_file", "root_file", "rewrite", "trim", "debug"],
@@ -19,11 +21,11 @@ user_input = initialize_macro(
 user_input = check_macro_config(user_input, debug=user_input["debug"])
 
 # Process data: root -> numpy #
-info = json.load(open("../config/" + user_input["config_file"] + ".json", "r"))
+info = json.load(open(f'{root}/config/{user_input["config_file"]}.json', "r"))
 for name in user_input["root_file"]:
     root_info = get_root_info(
-        info["NAME"] + name, info["PATH"], debug=user_input["debug"]
+        f'{info["NAME"]}{name}', f'{root}{info["PATH"]}', user_input, debug=user_input["debug"]
     )
     if user_input["rewrite"] == False:
         root_info = remove_processed_branches(root_info, debug=user_input["debug"])
-    root2npy(root_info, trim=user_input["trim"], debug=user_input["debug"])
+    root2npy(root_info, user_input, trim=user_input["trim"], debug=user_input["debug"])
