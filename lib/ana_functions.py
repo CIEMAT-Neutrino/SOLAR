75ce4dcd1df12a74de9dcf38827657b155d8ccbd
mantheys <sergio.manthey@ciemat.es>
2024-05-27 11:02:28 +0200

    UPDATE PATHS

--- lib/ana_functions.py
+++ lib/ana_functions.py
@@ -5,7 +5,7 @@ def get_default_nhits(root, variable:str = "NHITS"):
     '''
     This function returns the default energy binning used in the analysis.
     '''
-    analysis_info = json.load(open(f"{root}/import/analysis.json",'r'))
+    analysis_info = json.load(open(f"{root}/lib/import/analysis.json",'r'))
     nhits = analysis_info[f"{variable}"]
     return nhits
 
@@ -14,7 +14,7 @@ def get_default_energies(root, variable:str = "RECO_ENERGY"):
     '''
     This function returns the default energy binning used in the analysis.
     '''
-    analysis_info = json.load(open(f"{root}/import/analysis.json",'r'))
+    analysis_info = json.load(open(f"{root}/lib/import/analysis.json",'r'))
     e_range = analysis_info[f"{variable}_RANGE"]
     e_bins = analysis_info[f"{variable}_BINS"]
     energy_edges = np.linspace(e_range[0],e_range[-1],e_bins+1)
