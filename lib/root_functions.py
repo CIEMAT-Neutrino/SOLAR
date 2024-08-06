a5b68c98a0bc11760322791abfb797e6ead6f91b
mantheys <sergio.manthey@ciemat.es>
2024-08-06 14:09:01 +0200

    UPDATE HIST FITTER FOR SOLAR ANALYSIS

--- lib/root_functions.py
+++ lib/root_functions.py
@@ -25,7 +25,7 @@ def th2f_from_dataframe(df, name="myhist", title="My Histogram", debug=False):
     y_centers = df.index.to_numpy(dtype=float)
 
     # Convert DataFrame data to a 2D numpy array for z-axis values
-    z_values = df.to_numpy(dtype=float)
+    z_values = df.to_numpy(dtype=int)
 
     # Create a TH2F histogram
     nbins_x = len(x_centers)
@@ -35,60 +35,38 @@ def th2f_from_dataframe(df, name="myhist", title="My Histogram", debug=False):
     # Fill the TH2F histogram with z-axis values
     for i in range(nbins_x):
         for j in range(nbins_y):
-            th2f.SetBinContent(i + 1, j + 1, z_values[j][i])  # Note: ROOT histograms are filled in a column-major order
+            th2f.SetBinContent(i + 1, j + 1, int(z_values[j][i]))  # Note: ROOT histograms are filled in a column-major order
 
     if debug: print_colored("Created TH2F histogram: %s"%name,"INFO")
     return th2f
 
-def generate_synthetic_histograms():
-    '''
-    Create synthetic histograms for testing purposes.
-    '''
-    nbins_x = 10
-    nbins_y = 10
-
-    obs_values = [i + j for i in range(nbins_x) for j in range(nbins_y)]
-    solar_values = [2 * i for i in obs_values]
-    neut_values = [3 * i for i in obs_values]
-
-    obs_hist = ROOT.TH2F("obs", "Observed Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
-    solar_hist = ROOT.TH2F("solar", "Solar Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
-    neut_hist = ROOT.TH2F("neut", "Neutrino Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
-
-    for i in range(1, nbins_x + 1):
-        for j in range(1, nbins_y + 1):
-            obs_hist.SetBinContent(i, j, obs_values[(i - 1) * nbins_y + (j - 1)])
-            solar_hist.SetBinContent(i, j, solar_values[(i - 1) * nbins_y + (j - 1)])
-            neut_hist.SetBinContent(i, j, neut_values[(i - 1) * nbins_y + (j - 1)])
-
-    return obs_hist, solar_hist, neut_hist
 
 class Fitter:
     '''
     Class to fit the solar neutrino histograms for each set of oscillation parameters.
 
     Args:
-        obs (ROOT.TH2F): observed data histogram.
+        obs (ROOT.TH2F): observed (false-data) data histogram.
         solar (ROOT.TH2F): solar data histogram.
-        neut (ROOT.TH2F): neutrino data histogram.
-        DayNight (bool): True if Day-Night asymmetry is included, False otherwise (default: True).
-        SigmaSolar (float): uncertainty on the solar neutrino flux (default: 0.04).
-        SigmaNeut (float): uncertainty on the atmospheric neutrino flux (default: 0.02).
+        bkg (ROOT.TH2F): bkg data histogram.
+        DayNight (bool): True if Night asymmetry is included, False only include Day data (upturn) (default: True).
+        SigmaPred (float): uncertainty on the predicted neutrino flux (default: 0.04).
+        SigmaBkg (float): uncertainty on the background flux (default: 0.02).
 
     Returns:
         chisq (float): chi-squared value.
-        A_solar (float): best-fit value of the solar neutrino flux.
-        A_neut (float): best-fit value of the atmospheric neutrino flux.
+        A_pred (float): best-fit value of the predicted neutrino amplitude.
+        A_bkg (float): best-fit value of the background amplitude.
     '''
-    def __init__(self, obs, solar, neut, DayNight=True, SigmaSolar=0.04, SigmaNeut=0.02):
+    def __init__(self, obs, pred, bkg, DayNight=True, SigmaPred=0.04, SigmaBkg=0.02):
         self.fObs = obs
-        self.fSolar = solar
-        self.fNeut = neut
+        self.fPred = pred
+        self.fBkg = bkg
         self.fDayNight = DayNight
-        self.fSigmaSolar = SigmaSolar
-        self.fSigmaNeut = SigmaNeut
+        self.fSigmaPred = SigmaPred
+        self.fSigmaBkg = SigmaBkg
 
-    def ROOTOperator(self, A_solar, A_neut):
+    def ROOTOperator(self, A_pred, A_bkg):
         chisq = 0
         for i in range(1, self.fObs.GetNbinsX() + 1):
             for j in range(1, self.fObs.GetNbinsY() + 1):
@@ -96,38 +74,65 @@ class Fitter:
                     e = 0
                     o = 0
                 else:
-                    N_neut = (1 + A_neut) * self.fNeut.GetBinContent(i, j)
-                    N_solar = (1 + A_solar) * self.fSolar.GetBinContent(i, j)
+                    N_bkg = (1 + A_bkg) * self.fBkg.GetBinContent(i, j)
+                    N_pred = (1 + A_pred) * self.fPred.GetBinContent(i, j)
                 
-                    e = N_neut + N_solar
+                    e = N_bkg + N_pred
                     o = self.fObs.GetBinContent(i, j)
                 
-                if o == 0:
+                if e == 0:
                     continue
                 
                 if o == 0:
-                    chisq += 2 * (e - o)
+                    chisq += abs(2 * (e - o))
+                
                 else:
-                    chisq += 2 * (e - o + o * np.log(o / e))
+                    chisq += abs(2 * (e - o + o * np.log(o / e)))
 
-        chisq += ((A_solar) / self.fSigmaSolar)**2 + ((A_neut) / self.fSigmaNeut)**2
+        # Add penalty terms for the fit
+        chisq += ((A_pred) / self.fSigmaPred)**2 + ((A_bkg) / self.fSigmaBkg)**2
+
         return chisq
 
-    def Fit(self, initial_A_solar, initial_A_neut, verbose=0, debug=False):
+    def Fit(self, initial_A_pred, initial_A_bkg, verbose=0, debug=False):
         
         if type(self.fObs) == ROOT.TH2F:
-            m = Minuit(self.ROOTOperator, A_solar=initial_A_solar, A_neut=initial_A_neut)
+            m = Minuit(self.ROOTOperator, A_pred=initial_A_pred, A_bkg=initial_A_bkg)
 
         else:
             print_colored("ERROR: Unknown input type","ERROR")
             return -1, -1, -1
         
-        m.limits['A_solar'] = (initial_A_solar - 10, initial_A_solar + 10)
-        m.limits['A_neut'] = (initial_A_neut - 10, initial_A_neut + 10)
+        m.limits['A_pred'] = (initial_A_pred - 10, initial_A_pred + 10)
+        m.limits['A_bkg'] = (initial_A_bkg - 10, initial_A_bkg + 10)
 
         m.migrad()
 
-        A_solar = m.values['A_solar']
-        A_neut = m.values['A_neut']
+        A_pred = m.values['A_pred']
+        A_bkg = m.values['A_bkg']
 
-        return m.fval, A_solar, A_neut
+        return m.fval, A_pred, A_bkg
+    
+
+# def generate_synthetic_histograms():
+#     '''
+#     Create synthetic histograms for testing purposes.
+#     '''
+#     nbins_x = 10
+#     nbins_y = 10
+
+#     obs_values = [i + j for i in range(nbins_x) for j in range(nbins_y)]
+#     solar_values = [2 * i for i in obs_values]
+#     neut_values = [3 * i for i in obs_values]
+
+#     obs_hist = ROOT.TH2F("obs", "Observed Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
+#     solar_hist = ROOT.TH2F("solar", "Solar Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
+#     neut_hist = ROOT.TH2F("neut", "Neutrino Data", nbins_x, 0, nbins_x, nbins_y, 0, nbins_y)
+
+#     for i in range(1, nbins_x + 1):
+#         for j in range(1, nbins_y + 1):
+#             obs_hist.SetBinContent(i, j, obs_values[(i - 1) * nbins_y + (j - 1)])
+#             solar_hist.SetBinContent(i, j, solar_values[(i - 1) * nbins_y + (j - 1)])
+#             neut_hist.SetBinContent(i, j, neut_values[(i - 1) * nbins_y + (j - 1)])
+
+#     return obs_hist, solar_hist, neut_hist
