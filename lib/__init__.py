580dc4b451439c2f9b36dcc2fa4eae73df5cbc45
mantheys <sergio.manthey@ciemat.es>
2024-10-30 16:45:52 +0100

    CLEANUP INIT

--- lib/__init__.py
+++ lib/__init__.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_curve, auc, accuracy_score
 from scipy.interpolate import interp1d
 
+# Import all the local functions
 from .ana_functions import *
 from .df_functions import *
 from .fit_functions import *
@@ -24,16 +25,22 @@ from .solar_functions import *
 from .wkf_functions import *
 from .evt_functions import *
 
-root = get_project_root()
+# Config the external libraries
 np.seterr(divide="ignore", invalid="ignore")
 pd.options.mode.chained_assignment = None  # default='warn'
 pd.set_option('future.no_silent_downcasting', True)
 plt.rcParams.update({"font.size": 15})
 
+# Load the default values
+root = get_project_root()
+
 nhits = get_default_nhits(root)
+
 energy_edges, energy_centers, ebin = get_default_energies(root)
+
 red_energy_edges, red_energy_centers, red_ebin = get_default_energies(
     root, "REDUCED_RECO_ENERGY")
+
 lowe_energy_edges, lowe_energy_centers, lowe_ebin = get_default_energies(
     root, "LOWE_RECO_ENERGY")
 
