8c814b7cf753314c457c076394c98cf83d7873a9
mantheys <sergio.manthey@ciemat.es>
2024-12-20 11:55:33 +0100

    UPDATE PROCESSING SCRIPT TO INCLUDE DEFAULTS

--- src/macros/00Processing.py
+++ src/macros/00Processing.py
@@ -10,11 +10,11 @@ from lib import (
     get_project_root
 )
 
-default_dict = {}
+default_dict = {"rewrite": True, "trim": False, "debug": True}
 root = get_project_root()
 user_input = initialize_macro(
     "00Process",
-    ["config_file", "root_file", "rewrite", "trim", "debug"],
+    ["config_file", "root_prefix", "root_file", "rewrite", "trim", "debug"],
     default_dict=default_dict,
     debug=True,
 )
@@ -22,10 +22,9 @@ user_input = check_macro_config(user_input, debug=user_input["debug"])
 
 # Process data: root -> numpy #
 info = json.load(open(f'{root}/config/{user_input["config_file"]}.json', "r"))
-for name in user_input["root_file"]:
-    root_info = get_root_info(
-        name=f'{info["NAME"]}{name}', path=f'{info["PATH"]}/{info["GEOMETRY"]}/{info["VERSION"]}/', user_input=user_input, debug=user_input["debug"]
-    )
-    if user_input["rewrite"] == False:
-        root_info = remove_processed_branches(root_info, debug=user_input["debug"])
-    root2npy(root_info, user_input, trim=user_input["trim"], debug=user_input["debug"])
+root_info = get_root_info(
+    name=f'{user_input["root_prefix"]}{user_input["root_file"]}', path=f'{info["PATH"]}/{info["GEOMETRY"]}/{info["VERSION"]}/', user_input=user_input, debug=user_input["debug"]
+)
+if user_input["rewrite"] == False:
+    root_info = remove_processed_branches(root_info, debug=user_input["debug"])
+root2npy(root_info, user_input, trim=user_input["trim"], debug=user_input["debug"])
